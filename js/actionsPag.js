cc13b0d8593ded2e4ac3c8030fb4ce2f260d43fb
Juan Felipe Arias <juantrullo@gmail.com>
2016-05-11 13:56:19 -0500

    Signed-off-by: Juan Felipe Arias <juantrullo@gmail.com> - Se realiza el cambio para eliminar interfaz que no sirve. - Se agregan botones de limpiar y refrescar pantalla.

--- js/actionsPag.js
+++ js/actionsPag.js
@@ -5,14 +5,15 @@
  */
 
 function recognize_image(){
-    document.getElementById('transcription').innerText = "(Recognizing...)"
+    document.getElementById('transcription').innerText = "(Recognizing...)";
 
     OCRAD(document.getElementById("c"), {
-            numeric: true
+    //OCRAD(document.getElementById("laimagen"), {
+            numeric: true,
     }, function(text){
-            document.getElementById('transcription').className = "done"
+            document.getElementById('transcription').className = "done";
             document.getElementById('transcription').innerText = text;
-    })
+    });
 }
 function getImageCanvas(){
     var canvas = document.getElementById("c");
@@ -22,45 +23,45 @@ function getImageCanvas(){
 }
                     
 var c = document.getElementById('c'),
-            o = c.getContext('2d');
+    o = c.getContext('2d');
 
 function reset_canvas(){
-        o.fillStyle = 'white'
-        o.fillRect(0, 0, c.width, c.height)
-        o.fillStyle = 'black'	
+        o.fillStyle = 'white';
+        o.fillRect(0, 0, c.width, c.height);
+        o.fillStyle = 'black';	
 }
 
 // here's a really simple little drawing app so people can try their luck at
 // the lottery that is offline handwriting recognition
 var drag = false, lastX, lastY;
-c.onmousedown = function(e){ drag = true; lastX = 0; lastY = 0; e.preventDefault(); c.onmousemove(e) }
-c.onmouseup   = function(e){ drag = false; e.preventDefault(); runOCR() }
+c.onmousedown = function(e){ drag = true; lastX = 0; lastY = 0; e.preventDefault(); c.onmousemove(e); }
+c.onmouseup   = function(e){ drag = false; e.preventDefault(); runOCR(); }
 c.onmousemove = function(e){
-        e.preventDefault()
-        var rect = c.getBoundingClientRect();
-        var r = 5;
+    e.preventDefault();
+    var rect = c.getBoundingClientRect();
+    var r = 5;
 
-function dot(x, y){
-        o.beginPath()
-        o.moveTo(x + r, y)
-        o.arc(x, y, r, 0, Math.PI * 2)
-        o.fill()
-}
+    function dot(x, y){
+            o.beginPath();
+            o.moveTo(x + r, y);
+            o.arc(x, y, r, 0, Math.PI * 2);
+            o.fill();
+    }
     if(drag){
-            var x = e.clientX - rect.left, 
-                    y = e.clientY - rect.top;
+        var x = e.clientX - rect.left, 
+                y = e.clientY - rect.top;
 
-            if(lastX && lastY){
-                    var dx = x - lastX, dy = y - lastY;
-                    var d = Math.sqrt(dx * dx + dy * dy);
-                    for(var i = 1; i < d; i += 2){
-                            dot(lastX + dx / d * i, lastY + dy / d * i)
-                    }
+        if(lastX && lastY){
+            var dx = x - lastX, dy = y - lastY;
+            var d = Math.sqrt(dx * dx + dy * dy);
+            for(var i = 1; i < d; i += 2){
+                    dot(lastX + dx / d * i, lastY + dy / d * i);
             }
-            dot(x, y)
+        }
+        dot(x, y);
 
-            lastX = x;
-            lastY = y;
+        lastX = x;
+        lastY = y;
     }
 }
 
@@ -75,26 +76,26 @@ document.body.ondrop = function(e){
 }
 
 var lastWorker;
-var worker = new Worker('worker.js')
+var worker = new Worker('worker.js');
 function runOCR(image_data, raw_feed){
-        document.getElementById("output").className = 'processing'
+        document.getElementById("output").className = 'processing';
         worker.onmessage = function(e){
 
-                document.getElementById("output").className = ''
+                document.getElementById("output").className = '';
 
                 if('innerText' in document.getElementById("text")){
-                        document.getElementById("text").innerText = e.data
+                        document.getElementById("text").innerText = e.data;
                 }else{
-                        document.getElementById("text").textContent = e.data	
+                        document.getElementById("text").textContent = e.data;	
                 }
                 document.getElementById('timing').innerHTML = 'recognition took ' + ((Date.now() - start)/1000).toFixed(2) + 's';
         }
-        var start = Date.now()
+        var start = Date.now();
         if(!raw_feed){
-                image_data = o.getImageData(0, 0, c.width, c.height);	
+            image_data = o.getImageData(0, 0, c.width, c.height);	
         }
 
-        worker.postMessage(image_data)
+        worker.postMessage(image_data);
         lastWorker = worker;
 }
 
