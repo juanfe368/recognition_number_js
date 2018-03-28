d50497e5f4d3232e666a10c97d092b9770aac230
juanfe <juantrullo@gmail.com>
2018-03-27 22:57:05 -0500

    Se intenta arreglar wl webpack
    Se intenta arreglar el webpack pero no funciona.

--- js/actionsPag.bundlev1.js
+++ js/actionsPag.bundlev1.js
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+console.log("Nueva prueba");
 var numIntentos = 0;
 
 function recognize_image() {
--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,19 @@ var webpack = require('webpack');
 var path = require('path');
 const WebpackCleanupPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     entry: {
         numberjs: path.join(__dirname, './js/actionsPag.bundlev1.js')
     },
+    devtool: "source-map",
     output: {
         path: path.join(__dirname, './js/dist/'),
         filename: '[name].[chunkhash].bundle.js'
     },
     plugins: [
-        new WebpackCleanupPlugin(['./js/dist/'])
+        new WebpackCleanupPlugin(['./js/dist/']),
+        new UglifyJsPlugin()
     ]
 };
