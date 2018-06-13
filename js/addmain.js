9d32a99baf1f621138b8bc8ee1264911f6a2c89b
juanfe <juantrullo@gmail.com>
2018-06-12 22:48:44 -0500

    JFA-2018-06-12
    - Se realiza el cambio visual con resultados aleatorios para los ejercicios

--- js/addmain.js
+++ js/addmain.js
@@ -1,10 +1,23 @@
-var app = angular.module('myApp', ['ngSanitize']);
+/*angular.module('myApp', 'customInterpolationApp')*/
+var app = angular.module("myApp", []);
+/*app.controller("myCtrl", function($scope) {
+    $scope.records = [
+        "Alfreds Futterkiste",
+        "Berglunds snabbköp",
+        "Centro comercial Moctezuma",
+        "Ernst Handel",
+    ]
+});*/
+
 app.controller('myCtrl', function($scope) {
-    /*$scope.firstName = "John";
-    $scope.lastName = "Doe";*/
 
     $scope.misc = {};
-    $scope.data = {};
+    $scope.data = {
+      answeruser: "",
+      turnuser: 0,
+      answercorrect: 0,
+      answerinccorect: 0
+    };
 
     $scope.initialDataOperation = function(){
       $scope.misc.valueRandom = $scope.valueRandom();
@@ -14,15 +27,15 @@ app.controller('myCtrl', function($scope) {
       $scope.data.valueFinal = $scope.data.numbersadd[$scope.misc.valueRandom];
 
       $scope.data.answeroperation = $scope.data.valueInitial + $scope.data.valueFinal;
-      $scope.data.operation = {
-        valorInital: $scope.data.valueInitial,
-        valorFinal: $scope.data.valueFinal,
-        valorAnswer: $scope.data.answeroperation
-      };
+
+      $scope.data.operation = [];
+      $scope.data.operation = {valorInital:$scope.data.valueInitial, valorFinal:$scope.data.valueFinal, valueAnswer:$scope.data.answeroperation};
 
       $scope.data.answers = new Array();
-      $scope.data.answers.push($scope.data.operation);
-      while($scope.data.answers.length<4){
+      $scope.data.posAnswer = Math.floor((Math.random() * 4));
+      $scope.data.answers[$scope.data.posAnswer] = $scope.data.operation.valueAnswer;
+      var countPos = 0;
+      while(countPos<4){
         var valueAux1 = $scope.valueRandom();
         var valueAux2 = $scope.valueRandom();
         $scope.misc.valuesAux = {
@@ -30,20 +43,44 @@ app.controller('myCtrl', function($scope) {
           valorFinal: valueAux2,
           valueAnswer: valueAux1 + valueAux2
         };
-
-        if($scope.data.answers.indexOf($scope.data.answeroperation)==-1){
-          $scope.data.answers.push($scope.misc.valuesAux);
+        if($scope.data.answers.indexOf($scope.misc.valuesAux.valueAnswer)<0){
+          if($scope.data.answers[countPos]==null||$scope.data.answers[countPos]==""){
+            $scope.data.answers[countPos] = $scope.misc.valuesAux.valueAnswer;
+            countPos++;
+          }
+          else{
+            countPos++;
+          }
         }
-
+        //countPos++;
       }
-
     }
 
     $scope.valueRandom = function(){
       return Math.floor((Math.random() * 10));
     }
 
-    $scope.initialDataOperation();
+    $scope.chooseAnswer = function(){
 
-    console.log('operation', $scope.data.answers);
+      if($scope.data.answeruser==$scope.data.operation.valueAnswer){
+        $scope.data.answercorrect++;
+        console.log('La respuesta es correcta:',$scope.data.answeruser);
+      }
+      else{
+        $scope.data.answerinccorect++;
+        console.log('La respuesta es incorrecta:',$scope.data.answeruser);
+      }
+      $scope.data.turnuser++;
+      if($scope.data.turnuser==4){
+        console.log('Respuestas Correctas: ',$scope.data.answercorrect);
+        console.log('Respuestas incorrectas: ',$scope.data.answerinccorect);
+        console.log('Fin del juego');
+      }
+      else{
+        $scope.data.answeruser = "";
+        $scope.initialDataOperation();
+      }
+    }
+
+    $scope.initialDataOperation();
 });
